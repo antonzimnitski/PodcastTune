1200da1c0de47cd3b85f701d0e297f59ca3303d8
Zimniros <zimniros@protonmail.com>
2018-05-27 22:56:43 +0300

    Add fetch api polyfill to fix IE compatibility

--- imports/startup/client/index.js
+++ imports/startup/client/index.js
@@ -3,6 +3,7 @@ import React from "react";
 import { render } from "react-dom";
 import ApolloClient, { HttpLink, InMemoryCache } from "apollo-boost";
 import { ApolloProvider } from "react-apollo";
+import "whatwg-fetch";
 
 import App from "./../../ui/components/App";
 
