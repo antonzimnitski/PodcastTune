3df249cadfbb388acdda291c1f09b9b0113e0e63
Zimniros <zimniros@protonmail.com>
2018-06-10 15:18:52 +0300

    Add volume control, playback rate control and mute handle to AudioPlayer component

--- imports/ui/components/AudioPlayer.js
+++ imports/ui/components/AudioPlayer.js
@@ -13,9 +13,18 @@ class AudioPlayer extends Component {
       isPlaying: false,
       mounted: false,
       isLoading: true,
-      duration: 0
+      isMuted: false,
+      volume: 1,
+      duration: 0,
+      playedSeconds: 0,
+      playbackRate: 1,
+      minPlaybackRate: 0.5,
+      maxPlaybackRate: 4,
+      playbackStep: 0.1
     };
   }
+
+  _lastVolume = 0;
 
   componentDidMount() {
     const { episode } = this.props;
@@ -23,13 +32,33 @@ class AudioPlayer extends Component {
     if (episode) {
       this.player.current.src = episode.mediaUrl;
     }
+
+    this.getSeconds();
   }
+
+  componentWillUnmount() {
+    clearTimeout(this.getSecondsTimeout);
+  }
+
+  getSeconds = () => {
+    const { episode } = this.props;
+    if (
+      episode &&
+      this.player.current &&
+      this.state.isReady &&
+      this.state.isPlaying
+    ) {
+      console.log(this.player.current.currentTime);
+    }
+    this.getSecondsTimeout = setTimeout(this.getSeconds, 2000);
+  };
 
   componentWillReceiveProps(nextProps) {
     const { episode } = this.props;
 
     if (!episode || episode.mediaUrl !== nextProps.episode.mediaUrl) {
       this.setState({ isLoading: true });
+      this.setState({ isPlaying: false });
       this.player.current.src = nextProps.episode.mediaUrl;
     }
   }
@@ -37,19 +66,15 @@ class AudioPlayer extends Component {
   onReady() {
     this.setState({ isReady: true });
     this.setState({ isLoading: false });
-    console.log("onReady");
-    console.log("duration", this.player.current.duration);
-    this.setDuration();
+    this.onPlay();
   }
 
   onPlay() {
-    console.log("onPlay");
     this.setState({ isPlaying: true });
     this.player.current.play();
   }
 
   onPause() {
-    console.log("onPause");
     this.setState({ isPlaying: false });
     this.player.current.pause();
   }
@@ -62,14 +87,59 @@ class AudioPlayer extends Component {
     }
   }
 
+  increasePlaybackRate() {
+    const { playbackRate, playbackStep, maxPlaybackRate } = this.state;
+    //https://stackoverflow.com/questions/10473994/javascript-adding-decimal-numbers-issue#10474055
+    const newRate = +(playbackRate + playbackStep).toFixed(1);
+    if (newRate <= maxPlaybackRate) {
+      this.setPlaybackRate(newRate);
+    }
+  }
+
+  decreasePlaybackRate() {
+    const { playbackRate, playbackStep, minPlaybackRate } = this.state;
+    // https://stackoverflow.com/questions/10473994/javascript-adding-decimal-numbers-issue#10474055
+    const newRate = +(playbackRate - playbackStep).toFixed(1);
+    if (newRate >= minPlaybackRate) {
+      this.setPlaybackRate(newRate);
+    }
+  }
+
   setDuration() {
     this.setState({ duration: this.player.current.duration });
+  }
+
+  setPlaybackRate(playbackRate) {
+    this.setState({ playbackRate });
+    this.player.current.playbackRate = this.state.playbackRate;
+  }
+
+  onTimeChange(e) {
+    this.setState({ playedSeconds: e.target.value });
+    this.player.current.currentTime = this.state.playedSeconds;
+  }
+
+  onVolumeChange(e) {
+    this.setState({ volume: Number(e.target.value) });
+    console.log(this.state.volume);
+    this.player.current.volume = this.state.volume;
+  }
+
+  onMute() {
+    if (this.state.isMuted) {
+      this.setState({ isMuted: false });
+    } else {
+      this.setState({ isMuted: true });
+    }
+
+    this.player.current.muted = !this.state.isMuted;
   }
 
   render() {
     const { episode } = this.props;
     return (
       <div className="player">
+        <p>CurrentTime {this.state.playedSeconds}</p>
         <p>{episode ? episode.title : "Select episode to play"}</p>
         <p>{episode ? episode.author : "-"}</p>
         <p>
@@ -82,9 +152,49 @@ class AudioPlayer extends Component {
           {this.state.isPlaying ? "Pause" : "Play"}
         </button>
 
+        <div>
+          <span>
+            Duration bar instead of audio element {this.state.duration}
+          </span>
+          <input
+            onChange={e => this.onTimeChange(e)}
+            value={this.state.playedSeconds}
+            type="range"
+            min="0"
+            max={this.state.duration}
+            step="1"
+          />
+        </div>
+
+        <div>
+          <button onClick={() => this.decreasePlaybackRate()}>-</button>
+          <p>Current rate is {this.state.playbackRate}</p>
+          <button onClick={() => this.increasePlaybackRate()}>+</button>
+        </div>
+
+        <div />
+        <div>
+          <p>Volume control</p>
+          <button onClick={() => this.onMute()}>
+            {this.state.isMuted ? "Unmute" : "Mute"}
+          </button>
+          <input
+            onChange={e => this.onVolumeChange(e)}
+            value={this.state.volume}
+            type="range"
+            min="0"
+            max="1"
+            step="0.05"
+          />
+        </div>
+
         <audio
+          style={{ display: "none" }}
           ref={this.player}
+          onLoadedMetadata={() => this.setDuration()}
           onCanPlay={() => this.onReady()}
+          onPlay={() => this.onPlay()}
+          onPause={() => this.onPause()}
           preload="auto"
           controls
         />
