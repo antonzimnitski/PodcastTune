f3359edc5252fc0564cff80d80abb5aaa0caa07b
Zimniros <zimniros@protonmail.com>
2018-06-11 22:40:30 +0300

    Add AudioPlayer styles(desktop only)

--- imports/ui/components/AudioPlayer.js
+++ imports/ui/components/AudioPlayer.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { Session } from "meteor/session";
 import { withTracker } from "meteor/react-meteor-data";
+import moment from "moment";
+import momentDurationFormatSetup from "moment-duration-format";
 
 class AudioPlayer extends Component {
   constructor(props) {
@@ -48,7 +50,9 @@ class AudioPlayer extends Component {
       this.state.isReady &&
       this.state.isPlaying
     ) {
-      this.setState({ playedSeconds: this.player.current.currentTime });
+      this.setState({
+        playedSeconds: Math.floor(this.player.current.currentTime)
+      });
     }
     this.getSecondsTimeout = setTimeout(this.getSeconds, 1000);
   };
@@ -57,15 +61,13 @@ class AudioPlayer extends Component {
     const { episode } = this.props;
 
     if (!episode || episode.mediaUrl !== nextProps.episode.mediaUrl) {
-      this.setState({ isLoading: true });
-      this.setState({ isPlaying: false });
+      this.setState({ isLoading: true, isPlaying: false });
       this.player.current.src = nextProps.episode.mediaUrl;
     }
   }
 
   onReady() {
-    this.setState({ isReady: true });
-    this.setState({ isLoading: false });
+    this.setState({ isReady: true, isLoading: false });
     this.onPlay();
   }
 
@@ -116,7 +118,7 @@ class AudioPlayer extends Component {
   }
 
   setTime(value) {
-    this.setState({ playedSeconds: value }, () => {
+    this.setState({ playedSeconds: Math.floor(value) }, () => {
       this.player.current.currentTime = this.state.playedSeconds;
     });
   }
@@ -152,14 +154,6 @@ class AudioPlayer extends Component {
     const { duration, playedSeconds } = this.state;
     const newTime = +(playedSeconds + amount).toFixed(10);
     if (episode && this.player.current && this.state.isReady) {
-      console.log(
-        "duration",
-        duration,
-        "playedSeconds",
-        playedSeconds,
-        "newTime",
-        newTime
-      );
       if (newTime <= 0) {
         this.setTime(0);
       } else if (newTime >= duration) {
@@ -175,59 +169,147 @@ class AudioPlayer extends Component {
     this.onMute(!this.state.isMuted);
   }
 
+  formatSeconds(seconds) {
+    //TODO fix 0 seconds text
+    return moment.duration(seconds, "seconds").format();
+  }
+
   render() {
     const { episode } = this.props;
     return (
       <div className="player">
-        <button onClick={() => this.skipTime(-10)}>Skip -10</button>
-        <button onClick={() => this.skipTime(30)}>Skip +30</button>
-        <p>PlayedSeconds {this.state.playedSeconds}</p>
-        <p>{episode ? episode.title : "Select episode to play"}</p>
-        <p>{episode ? episode.author : "-"}</p>
-        <p>
-          Duration
-          {this.state.isReady && !this.state.isLoading
-            ? this.state.duration
-            : "--/--"}
-        </p>
-        <button onClick={() => this.handlePlayPause()}>
-          {this.state.isPlaying ? "Pause" : "Play"}
-        </button>
-
-        <div>
-          <span>
-            Duration bar instead of audio element {this.state.duration}
-          </span>
-          <input
-            onChange={e => this.setTime(Number(e.target.value))}
-            value={this.state.playedSeconds}
-            type="range"
-            min="0"
-            max={this.state.duration}
-            step="1"
-          />
+        <div className="player__controls-left">
+          <div
+            onClick={() => this.skipTime(-15)}
+            className="player__skip player__skip-back"
+          >
+            <span className="player__skip-text">15</span>
+          </div>
+          <div>
+            <svg
+              className={
+                this.state.isPlaying
+                  ? "player__play player__play--playing"
+                  : "player__play"
+              }
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 250 250"
+            >
+              <g onClick={() => this.handlePlayPause()} id="icon">
+                <g id="circle">
+                  <circle
+                    className="player__play-circle"
+                    cx="125"
+                    cy="125"
+                    r="115"
+                    fill="#fff"
+                  />
+                  <path d="M125,20A105,105,0,1,1,20,125,105.12,105.12,0,0,1,125,20m0-20A125,125,0,1,0,250,125,125,125,0,0,0,125,0Z" />
+                </g>
+                <g className="play__inner" id="inner">
+                  <g className="player__play-bars" id="bars">
+                    <g id="left">
+                      <rect
+                        x="92.5"
+                        y="87.5"
+                        width="15"
+                        height="75"
+                        fill="#fff"
+                      />
+                      <polygon points="117.5 77.5 82.5 77.5 82.5 172.5 117.5 172.5 117.5 77.5 117.5 77.5" />
+                    </g>
+                    <g id="right">
+                      <rect
+                        x="142.5"
+                        y="87.5"
+                        width="15"
+                        height="75"
+                        fill="#fff"
+                      />
+                      <polygon points="167.5 77.5 132.5 77.5 132.5 172.5 167.5 172.5 167.5 77.5 167.5 77.5" />
+                    </g>
+                  </g>
+                  <path
+                    className="player__play-triangle"
+                    id="triangle"
+                    d="M183.25,125,95.87,175.45V74.55Z"
+                  />
+                </g>
+              </g>
+            </svg>
+          </div>
+          <div
+            onClick={() => this.skipTime(30)}
+            className="player__skip player__skip-forward"
+          >
+            <span className="player__skip-text">30</span>
+          </div>
         </div>
 
-        <div>
-          <button onClick={() => this.decreasePlaybackRate()}>-</button>
-          <p>Current rate is {this.state.playbackRate}</p>
-          <button onClick={() => this.increasePlaybackRate()}>+</button>
+        <div className="player__controls-center">
+          <div className="player__title">
+            <span>{episode ? episode.title : "Select episode to play"}</span>
+          </div>
+          <div className="player__author">
+            <span>
+              {episode
+                ? `${episode.author} - ${moment(episode.pubDate).format(
+                    "MMM D, YYYY"
+                  )}`
+                : "-"}
+            </span>
+          </div>
+          <div className="player__seek-bar">
+            <input
+              className="seek-bar__range"
+              onChange={e => this.setTime(Number(e.target.value))}
+              value={this.state.playedSeconds}
+              type="range"
+              min="0"
+              max={this.state.duration}
+              step="1"
+            />
+            <span className="seek-bar__text seek-bar__text--left">
+              {this.state.isReady && !this.state.isLoading
+                ? this.formatSeconds(this.state.playedSeconds)
+                : "--/--"}
+            </span>
+            <span className="seek-bar__text seek-bar__text--right">
+              {this.state.isReady && !this.state.isLoading
+                ? this.formatSeconds(this.state.duration)
+                : "--/--"}
+            </span>
+          </div>
         </div>
-
-        <div />
-        <div>
-          <p>Volume control</p>
-          <button onClick={() => this.handleMute()}>
-            {this.state.isMuted ? "Unmute" : "Mute"}
-          </button>
-          <input
-            onChange={e => this.setVolume(Number(e.target.value))}
-            value={this.state.volume}
-            type="range"
-            min="0"
-            max="1"
-            step="0.1"
-          />
+        <div className="player__controls-right">
+          <div className="player__playback-rate">
+            <button className="playback-rate__button" onClick={() => this.decreasePlaybackRate()}>-</button>
+            <div className="playback-rate__text">{this.state.playbackRate}x</div>
+            <button className="playback-rate__button" onClick={() => this.increasePlaybackRate()}>+</button>
+          </div>
+          <div className={this.state.isMuted ? "player__volume player__volume--muted" : "player__volume"} >
+            <svg className="volume__icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 555.17 442.85">
+              <g onClick={() => this.handleMute()}> <g  className="volume__megaphone" id="volume">
+                <polygon  points="145.86 320.5 0.5 320.5 0.5 121.5 147.1 121.5 323.5 0.95 323.5 441.9 145.86 320.5"/>
+                <path d="M323,1.89V441L146.27,320.17,146,320H1V122H147.26l.25-.17L323,1.89M324,0,147,121H0V321H145.7L324,442.85V0Z"/>
+              </g>
+              <g className="volume__cross" id="cross">
+                <polygon points="459 251.41 393.95 316.46 363.54 286.05 428.59 221 363.54 155.95 393.95 125.54 459 190.59 524.05 125.54 554.46 155.95 489.41 221 554.46 286.05 524.05 316.46 459 251.41"/>
+                <path d="M394,126.25l64.34,64.34.71.71.71-.71,64.34-64.34,29.7,29.7-64.34,64.34-.71.71.71.71,64.34,64.34-29.7,29.7-64.34-64.34-.71-.71-.71.71L394,315.75l-29.7-29.7,64.34-64.34.71-.71-.71-.71L364.25,156l29.7-29.7m130.1-1.42-65,65.06L394,124.83,362.83,156,427.89,221l-65.06,65.05L394,317.17,459,252.11l65,65.06,31.12-31.12L490.11,221,555.17,156l-31.12-31.12Z"/>
+              </g>
+            </g>
+          </svg>
+            <div className="volume__slider">
+            <input
+              onChange={e => this.setVolume(Number(e.target.value))}
+              value={this.state.volume}
+              type="range"
+              min="0"
+              max="1"
+              step="0.1"
+            />
+            </div>
+          </div>
         </div>
 
         <audio
