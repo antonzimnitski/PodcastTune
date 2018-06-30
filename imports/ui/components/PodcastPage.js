8f1fa83b84bfc9cb3d3f12de17d480e9b48d1d4a
Zimniros <zimniros@protonmail.com>
2018-07-01 00:09:01 +0300

    Refactor graphql request to Query component in PodcastPage

--- imports/ui/components/PodcastPage.js
+++ imports/ui/components/PodcastPage.js
@@ -1,70 +1,87 @@
 import React, { Component } from "react";
-import { graphql } from "react-apollo";
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
 
 import InnerHeader from "./InnerHeader";
 import Feed from "./Feed";
 import Loader from "./helpers/Loader";
-import fetchPodcast from "./../queries/fetchPodcast";
+
+const GET_PODCAST = gql`
+  query Podcast($podcastId: Int!) {
+    podcast(podcastId: $podcastId) {
+      podcastId
+      feedUrl
+      title
+      author
+      website
+      description
+      summary
+      artworkUrl
+    }
+  }
+`;
 
 class PodcastPage extends Component {
   renderPodcast() {
-    const { podcast, error } = this.props.data;
-    if (!podcast || error) {
-      return <div>Sorry! There was an error loading your podcast.</div>;
-    }
     return (
-      <div className="podcast">
-        <div className="podcast__header">
-          <div>
-            <img
-              className="podcast__image"
-              src={podcast.artworkUrl}
-              alt={`${podcast.title} artwork`}
-            />
-          </div>
-          <div className="podcast__info">
-            <h1 className="podcast__title">{podcast.title}</h1>
-            <h2>
-              <a
-                className="podcast__link"
-                href={podcast.website}
-                target="_blank"
-              >
-                {podcast.author}
-              </a>
-            </h2>
-            <p
-              className="podcast__description"
-              dangerouslySetInnerHTML={{
-                __html: podcast.description || podcast.summary
-              }}
-            />
-          </div>
-        </div>
+      <Query
+        query={GET_PODCAST}
+        variables={{
+          podcastId: this.props.match.params.podcastId
+        }}
+      >
+        {({ loading, error, data }) => {
+          if (loading) return <Loader />;
+          if (error || !data.podcast) {
+            return <div>Sorry! There was an error loading your podcast.</div>;
+          }
+          const { podcast } = data;
+          return (
+            <div className="podcast">
+              <div className="podcast__header">
+                <div>
+                  <img
+                    className="podcast__image"
+                    src={podcast.artworkUrl}
+                    alt={`${podcast.title} artwork`}
+                  />
+                </div>
+                <div className="podcast__info">
+                  <h1 className="podcast__title">{podcast.title}</h1>
+                  <h2>
+                    <a
+                      className="podcast__link"
+                      href={podcast.website}
+                      target="_blank"
+                    >
+                      {podcast.author}
+                    </a>
+                  </h2>
+                  <p
+                    className="podcast__description"
+                    dangerouslySetInnerHTML={{
+                      __html: podcast.description || podcast.summary
+                    }}
+                  />
+                </div>
+              </div>
 
-        <Feed podcastId={podcast.podcastId} />
-      </div>
+              <Feed podcastId={podcast.podcastId} />
+            </div>
+          );
+        }}
+      </Query>
     );
   }
 
   render() {
-    const content = this.props.data.loading ? <Loader /> : this.renderPodcast();
-
     return (
       <React.Fragment>
         <InnerHeader />
-        {content}
+        {this.renderPodcast()}
       </React.Fragment>
     );
   }
 }
 
-export default graphql(fetchPodcast, {
-  options: props => {
-    return {
-      variables: {
-        podcastId: props.match.params.podcastId
-      }
-    };
-  }
-})(PodcastPage);
+export default PodcastPage;
