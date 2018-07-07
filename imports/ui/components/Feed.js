5b15c92d0e331e6fc13032637e0bca714a9bfd98
Zimniros <zimniros@protonmail.com>
2018-07-07 15:41:50 +0300

    Refactor Feed component. Add Episode component

--- imports/ui/components/Feed.js
+++ imports/ui/components/Feed.js
@@ -1,19 +1,20 @@
 import React, { Component } from "react";
 import { Session } from "meteor/session";
-import moment from "moment";
-import momentDurationFormatSetup from "moment-duration-format";
 import EpisodeModal from "./helpers/EpisodeModal";
 import { Query, graphql, compose } from "react-apollo";
 import updateCurrentEpisode from "./../queries/updateCurrentEpisode";
 import getCurrentEpisode from "./../queries/getCurrentEpisode";
 import gql from "graphql-tag";
-import Loader from "./helpers/Loader";
 import { withTracker } from "meteor/react-meteor-data";
+
+import Loader from "./helpers/Loader";
+import Episode from "./helpers/Episode";
 
 const GET_FEED = gql`
   query Feed($podcastId: Int!, $offset: Int!, $limit: Int!) {
     feed(podcastId: $podcastId, offset: $offset, limit: $limit) {
       id
+      podcastId
       title
       pubDate
       duration
@@ -67,6 +68,7 @@ class Feed extends Component {
     };
 
     this.handleEpisodeModal = this.handleEpisodeModal.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
 
   _offset = 0;
@@ -105,20 +107,6 @@ class Feed extends Component {
     this.setState({ isModalOpen: !this.state.isModalOpen, id });
   }
 
-  formatDate(date) {
-    if (moment(date).isValid()) {
-      const format =
-        moment(date).year() === moment().year() ? "MMM D" : "MMM D, YYYY";
-      return moment(date).format(format);
-    }
-    return "";
-  }
-
-  formatDuration(seconds) {
-    if (!seconds) return "";
-    return moment.duration(seconds, "seconds").format();
-  }
-
   /* const className =
         this.props.queue &&
         isEqual(this.props.queue[0].title, episode.title)
@@ -145,74 +133,12 @@ class Feed extends Component {
               {data.feed.map(episode => {
                 if (!episode) return;
                 return (
-                  <div key={episode.id} className="episode">
-                    <div
-                      onClick={() => this.handleEpisodeModal(episode.id)}
-                      className="episode__title"
-                    >
-                      <p>{episode.title}</p>
-                    </div>
-                    <div className="episode__pub-date">
-                      <p>{this.formatDate(episode.pubDate)}</p>
-                    </div>
-                    <div className="episode__duration">
-                      <p>{this.formatDuration(episode.duration)}</p>
-                    </div>
-                    <div className="episode__controls">
-                      <svg
-                        className="controls__play"
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 250 250"
-                      >
-                        <g
-                          onClick={() =>
-                            this.handleClick(episode.id, this.props.podcastId)
-                          }
-                          id="icon"
-                        >
-                          <g id="circle">
-                            <circle
-                              className="play__circle"
-                              cx="125"
-                              cy="125"
-                              r="115"
-                              fill="#fff"
-                            />
-                            <path d="M125,20A105,105,0,1,1,20,125,105.12,105.12,0,0,1,125,20m0-20A125,125,0,1,0,250,125,125,125,0,0,0,125,0Z" />
-                          </g>
-                          <g className="play__inner" id="inner">
-                            <g className="play__bars" id="bars">
-                              <g id="left">
-                                <rect
-                                  x="92.5"
-                                  y="87.5"
-                                  width="15"
-                                  height="75"
-                                  fill="#fff"
-                                />
-                                <polygon points="117.5 77.5 82.5 77.5 82.5 172.5 117.5 172.5 117.5 77.5 117.5 77.5" />
-                              </g>
-                              <g id="right">
-                                <rect
-                                  x="142.5"
-                                  y="87.5"
-                                  width="15"
-                                  height="75"
-                                  fill="#fff"
-                                />
-                                <polygon points="167.5 77.5 132.5 77.5 132.5 172.5 167.5 172.5 167.5 77.5 167.5 77.5" />
-                              </g>
-                            </g>
-                            <path
-                              className="play__triangle"
-                              id="triangle"
-                              d="M183.25,125,95.87,175.45V74.55Z"
-                            />
-                          </g>
-                        </g>
-                      </svg>
-                    </div>
-                  </div>
+                  <Episode
+                    key={episode.id}
+                    episode={episode}
+                    handleEpisodeModal={this.handleEpisodeModal}
+                    handleClick={this.handleClick}
+                  />
                 );
               })}
 
---
+++ imports/ui/components/helpers/Episode.js
@@ -0,0 +1,78 @@
+import React from "react";
+import moment from "moment";
+import momentDurationFormatSetup from "moment-duration-format";
+
+const Episode = ({ episode, handleEpisodeModal, handleClick }) => {
+  return (
+    <div className="episode">
+      <div
+        onClick={() => handleEpisodeModal(episode.id)}
+        className="episode__title"
+      >
+        <p>{episode.title}</p>
+      </div>
+      <div className="episode__pub-date">
+        <p>{formatDate(episode.pubDate)}</p>
+      </div>
+      <div className="episode__duration">
+        <p>{formatDuration(episode.duration)}</p>
+      </div>
+      <div className="episode__controls">
+        <svg
+          className="controls__play"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 250 250"
+        >
+          <g
+            onClick={() => handleClick(episode.id, episode.podcastId)}
+            id="icon"
+          >
+            <g id="circle">
+              <circle
+                className="play__circle"
+                cx="125"
+                cy="125"
+                r="115"
+                fill="#fff"
+              />
+              <path d="M125,20A105,105,0,1,1,20,125,105.12,105.12,0,0,1,125,20m0-20A125,125,0,1,0,250,125,125,125,0,0,0,125,0Z" />
+            </g>
+            <g className="play__inner" id="inner">
+              <g className="play__bars" id="bars">
+                <g id="left">
+                  <rect x="92.5" y="87.5" width="15" height="75" fill="#fff" />
+                  <polygon points="117.5 77.5 82.5 77.5 82.5 172.5 117.5 172.5 117.5 77.5 117.5 77.5" />
+                </g>
+                <g id="right">
+                  <rect x="142.5" y="87.5" width="15" height="75" fill="#fff" />
+                  <polygon points="167.5 77.5 132.5 77.5 132.5 172.5 167.5 172.5 167.5 77.5 167.5 77.5" />
+                </g>
+              </g>
+              <path
+                className="play__triangle"
+                id="triangle"
+                d="M183.25,125,95.87,175.45V74.55Z"
+              />
+            </g>
+          </g>
+        </svg>
+      </div>
+    </div>
+  );
+};
+
+function formatDate(date) {
+  if (moment(date).isValid()) {
+    const format =
+      moment(date).year() === moment().year() ? "MMM D" : "MMM D, YYYY";
+    return moment(date).format(format);
+  }
+  return "";
+}
+
+function formatDuration(seconds) {
+  if (!seconds) return "";
+  return moment.duration(seconds, "seconds").format();
+}
+
+export default Episode;
