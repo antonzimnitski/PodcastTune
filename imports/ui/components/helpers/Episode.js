99120967b7a3e2d361410616fb698e122270d3bf
Zimniros <zimniros@protonmail.com>
2018-07-27 15:01:33 +0300

    Refactor Episode component

--- imports/ui/components/helpers/Episode.js
+++ imports/ui/components/helpers/Episode.js
@@ -160,38 +160,46 @@ class Episode extends Component {
       : this.localUpnext(id, podcastId, isInUpNext);
   }
 
+  removeFromCache(proxy, query, field, returnedObj) {
+    const { client } = this.props;
+    try {
+      const data = proxy.readQuery({ query });
+      remove(data[field], n => n.id === returnedObj.id);
+      proxy.writeQuery({ query, data });
+    } catch (e) {
+      client.query({ query });
+      console.log("query haven't been called", query);
+    }
+  }
+
+  addToCache(proxy, query, field, returnedObj) {
+    const { client } = this.props;
+    try {
+      const data = proxy.readQuery({ query });
+      remove(data.upnext, n => n.id === returnedObj.id);
+      data[field]
+        ? data[field].push(returnedObj)
+        : (data[field] = [returnedObj]);
+      proxy.writeQuery({ query, data });
+    } catch (e) {
+      client.query({ query });
+      console.log("query haven't been called", query);
+    }
+  }
+
   loggedUpnext(id, podcastId, isInUpNext) {
-    const { removeFromUpnext, addToUpnext, client } = this.props;
+    const { removeFromUpnext, addToUpnext } = this.props;
 
     isInUpNext
       ? removeFromUpnext({
           variables: { id, podcastId },
-          update: (proxy, { data: { removeFromUpnext } }) => {
-            try {
-              const data = proxy.readQuery({ query: getUpnext });
-              remove(data.upnext, n => n.id === removeFromUpnext.id);
-              proxy.writeQuery({ query: getUpnext, data });
-            } catch (e) {
-              client.query({ query: getUpnext });
-              console.log("query haven't been called", e);
-            }
-          }
+          update: (proxy, { data: { removeFromUpnext } }) =>
+            this.removeFromCache(proxy, getUpnext, "upnext", removeFromUpnext)
         }).catch(err => console.log("Error in removeFromUpnext", err))
       : addToUpnext({
           variables: { id, podcastId },
-          update: (proxy, { data: { addToUpnext } }) => {
-            try {
-              const data = proxy.readQuery({ query: getUpnext });
-              remove(data.upnext, n => n.id === removeFromUpnext.id);
-              data.upnext
-                ? data.upnext.push(addToUpnext)
-                : (data.upnext = [addToUpnext]);
-              proxy.writeQuery({ query: getUpnext, data });
-            } catch (e) {
-              client.query({ query: getUpnext });
-              console.log("query haven't been called", e);
-            }
-          }
+          update: (proxy, { data: { addToUpnext } }) =>
+            this.addToCache(proxy, getUpnext, "upnext", addToUpnext)
         }).catch(err => console.log("Error in addToUpnext", err));
   }
 
@@ -208,36 +216,27 @@ class Episode extends Component {
   }
 
   handleFavorites(id, podcastId, isInFavorites) {
-    const { removeFromFavorites, addToFavorites, client } = this.props;
+    const { removeFromFavorites, addToFavorites } = this.props;
     isInFavorites
       ? removeFromFavorites({
           variables: { id, podcastId },
-          update: (proxy, { data: { removeFromFavorites } }) => {
-            try {
-              const data = proxy.readQuery({ query: getFavorites });
-              remove(data.favorites, n => n.id === removeFromFavorites.id);
-              proxy.writeQuery({ query: getFavorites, data });
-            } catch (e) {
-              client.query({ query: getFavorites });
-              console.log("query haven't been called", e);
-            }
-          }
+          update: (proxy, { data: { removeFromFavorites } }) =>
+            this.removeFromCache(
+              proxy,
+              getFavorites,
+              "favorites",
+              removeFromFavorites
+            )
         }).catch(err => console.log("Error in removeFromFavorites", err))
       : addToFavorites({
           variables: { id, podcastId },
-          update: (proxy, { data: { addToFavorites } }) => {
-            try {
-              const data = proxy.readQuery({ query: getFavorites });
-              data.favorites
-                ? data.favorites.push(addToFavorites)
-                : (data.favorites = [addToFavorites]);
-
-              proxy.writeQuery({ query: getFavorites, data });
-            } catch (e) {
-              client.query({ query: getFavorites });
-              console.log("query haven't been called", e);
-            }
-          }
+          update: (proxy, { data: { addToFavorites } }) =>
+            this.removeFromCache(
+              proxy,
+              getFavorites,
+              "favorites",
+              addToFavorites
+            )
         }).catch(err => console.log("Error in addToFavorites", err));
   }
 
@@ -328,16 +327,16 @@ class Episode extends Component {
           >
             <svg
               className="controls__status-icon"
+              viewBox="0 0 406 299"
               xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 405.72 298.71"
             >
               <path
                 className="status-icon__unplayed"
-                d="M398.42 7.29a25.07 25.07 0 0 0-35.35 0L131.5 238.86 42.65 150a25.08 25.08 0 0 0-35.36 0 25.08 25.08 0 0 0 0 35.36l106.07 106.06a25.09 25.09 0 0 0 31.36 3.25 25 25 0 0 0 6.22-4.54L398.42 42.65a25.07 25.07 0 0 0 0-35.36z"
+                d="M398 7a25 25 0 0 0-35 0L132 239 43 150a25 25 0 0 0-35 0 25 25 0 0 0 0 35l106 106a25 25 0 0 0 31 3 25 25 0 0 0 6-5L398 43a25 25 0 0 0 0-35z"
               />
               <path
                 className="status-icon__played"
-                d="M238.07 149L344.13 42.94a25.07 25.07 0 0 0 0-35.36 25.07 25.07 0 0 0-35.35 0L202.71 113.65 96.65 7.58a25.08 25.08 0 0 0-35.36 0 25.08 25.08 0 0 0 0 35.36L167.36 149 61.29 255.07a25.07 25.07 0 0 0 0 35.35 25.07 25.07 0 0 0 35.36 0l106.06-106.06 106.07 106.06a25.05 25.05 0 0 0 35.35 0 25.05 25.05 0 0 0 0-35.35z"
+                d="M238 149L344 43a25 25 0 0 0 0-35 25 25 0 0 0-35 0L203 114 97 8a25 25 0 0 0-35 0 25 25 0 0 0 0 35L167 149 61 255a25 25 0 0 0 0 35 25 25 0 0 0 35 0l106-106 106 106a25 25 0 0 0 35 0 25 25 0 0 0 0-35z"
               />
             </svg>
           </div>
@@ -350,72 +349,44 @@ class Episode extends Component {
           >
             <svg
               className="controls__up-next"
+              viewBox="0 0 179 153"
               xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 179 152.5"
             >
               <path
                 className="up-next__add"
-                d="M173 30.21h-24.21V6a6 6 0 1 0-12.08 0v24.21h-24.17a6.06 6.06 0 0 0-6 6 6.06 6.06 0 0 0 6 6h24.17v24.25a6 6 0 1 0 12.08 0V42.29H173a6.06 6.06 0 0 0 6-6 6.06 6.06 0 0 0-6-6.08z"
+                d="M173 30h-24V6a6 6 0 1 0-12 0v24h-24a6 6 0 0 0-6 6 6 6 0 0 0 6 6h24v24a6 6 0 1 0 12 0V42H173a6 6 0 0 0 6-6 6 6 0 0 0-6-6z"
               />
-              <rect y="77.5" width="150" height="25" rx="12.5" ry="12.5" />
-              <rect y="127.5" width="150" height="25" rx="12.5" ry="12.5" />
-              <rect y="27.5" width="100" height="25" rx="12.5" ry="12.5" />
+              <rect y="78" width="150" height="25" rx="13" ry="13" />
+              <rect y="128" width="150" height="25" rx="13" ry="13" />
+              <rect y="28" width="100" height="25" rx="13" ry="13" />
               <path
                 className="up-next__remove"
-                d="M162.44 5.62l-20.13 20.12-20.13-20.12a7.11 7.11 0 0 0-10.06 10.06l20.12 20.13-20.12 20.13a7.13 7.13 0 0 0 0 10.06 7.13 7.13 0 0 0 10.06 0l20.13-20.13L162.44 66a7.11 7.11 0 0 0 10.06-10.06l-20.13-20.13 20.13-20.13a7.13 7.13 0 0 0 0-10.06 7.13 7.13 0 0 0-10.06 0z"
+                d="M162 6l-20 20-20-20a7 7 0 0 0-10 10l20 20-20 20a7 7 0 0 0 0 10 7 7 0 0 0 10 0l20-20L162 66a7 7 0 0 0 10-10l-20-20 20-20a7 7 0 0 0 0-10 7 7 0 0 0-10 0z"
               />
             </svg>
           </div>
-          <svg
-            className="controls__play"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 250 250"
+
+          <div
+            onClick={() => this.handleClick(id, podcastId, isPlayingEpisode)}
           >
-            <g
-              onClick={() => this.handleClick(id, podcastId, isPlayingEpisode)}
-              id="icon"
+            <svg
+              className="controls__play"
+              viewBox="0 0 250 250"
+              xmlns="http://www.w3.org/2000/svg"
             >
-              <g id="circle">
-                <circle
-                  className="play__circle"
-                  cx="125"
-                  cy="125"
-                  r="115"
-                  fill="#fff"
-                />
-                <path d="M125,20A105,105,0,1,1,20,125,105.12,105.12,0,0,1,125,20m0-20A125,125,0,1,0,250,125,125,125,0,0,0,125,0Z" />
-              </g>
-              <g className="play__inner" id="inner">
-                <g className="play__bars" id="bars">
-                  <g id="left">
-                    <rect
-                      x="92.5"
-                      y="87.5"
-                      width="15"
-                      height="75"
-                      fill="#fff"
-                    />
-                    <polygon points="117.5 77.5 82.5 77.5 82.5 172.5 117.5 172.5 117.5 77.5 117.5 77.5" />
-                  </g>
-                  <g id="right">
-                    <rect
-                      x="142.5"
-                      y="87.5"
-                      width="15"
-                      height="75"
-                      fill="#fff"
-                    />
-                    <polygon points="167.5 77.5 132.5 77.5 132.5 172.5 167.5 172.5 167.5 77.5 167.5 77.5" />
-                  </g>
+              <circle className="play__circle" cx="125" cy="125" r="115" />
+              <path d="M125,20A105,105,0,1,1,20,125,105,105,0,0,1,125,20m0-20A125,125,0,1,0,250,125,125,125,0,0,0,125,0Z" />
+              <g className="play__inner">
+                <g className="play__bars">
+                  <rect x="93" y="88" width="15" height="75" />
+                  <polygon points="118 78 83 78 83 173 118 173 118 78" />
+                  <rect x="143" y="88" width="15" height="75" />
+                  <polygon points="168 78 133 78 133 173 168 173 168 78" />
                 </g>
-                <path
-                  className="play__triangle"
-                  id="triangle"
-                  d="M183.25,125,95.87,175.45V74.55Z"
-                />
+                <path className="play__triangle" d="m183 125-87 50v-101z" />
               </g>
-            </g>
-          </svg>
+            </svg>
+          </div>
         </div>
       </div>
     );
