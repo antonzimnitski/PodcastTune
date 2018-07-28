844d65ead43d5e08ae992b3e86f955f8d67d0e21
Zimniros <zimniros@protonmail.com>
2018-07-28 20:03:38 +0300

    Put bar.svg and x.svg into Header component.

--- imports/ui/components/Header.js
+++ imports/ui/components/Header.js
@@ -4,14 +4,14 @@ import { withTracker } from "meteor/react-meteor-data";
 import { Link } from "react-router-dom";
 
 const Header = props => {
-  const navImageSrc = props.isNavOpen ? "/images/x.svg" : "/images/bars.svg";
   return (
     <div className="top-header">
-      <img
+      <div
         className="top-header__nav-toogle"
         onClick={() => props.handleNavToggle()}
-        src={navImageSrc}
-      />
+      >
+        {props.isNavOpen ? xSvg : barsSvg}
+      </div>
       <Link className="top-header__logo" to="/">
         <img src="/images/logo.svg" alt="" />
       </Link>
@@ -25,3 +25,48 @@ export default withTracker(() => {
     isNavOpen: Session.get("isNavOpen")
   };
 })(Header);
+
+const barsSvg = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="200"
+    height="200"
+    viewBox="0 0 200 200"
+  >
+    <g fill="none">
+      <g fill="#fafafa">
+        <rect y="27" width="200" height="25.2" />
+        <rect y="87.3" width="200" height="25.2" />
+        <rect y="147.6" width="200" height="25.2" />
+      </g>
+    </g>
+  </svg>
+);
+
+const xSvg = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="200"
+    height="200"
+    viewBox="0 0 200 200"
+  >
+    <g fill="none">
+      <g fill="#fafafa">
+        <rect
+          transform="translate(100.171201 99.58)rotate(45)"
+          x="-124.2"
+          y="-12.6"
+          width="248.3"
+          height="25.2"
+        />
+        <rect
+          transform="translate(100.171201 99.58)rotate(-45)"
+          x="-124.2"
+          y="-12.6"
+          width="248.3"
+          height="25.2"
+        />
+      </g>
+    </g>
+  </svg>
+);
